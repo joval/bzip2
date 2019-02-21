afb160f54be15d4bf1d72af87128855ddea91cfc
David Solin <solin@farnamhallventures.com>
2019-02-20 21:06:14 -0600

    Source code.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bzip2
 The bzip2 implementation from Apache Ant.
+
+See also: http://www.kohsuke.org/bzip2/
---
+++ classes.mk
@@ -0,0 +1,5 @@
+CLASSES=\
+	org.apache.tools.bzip2.CBZip2InputStream	\
+	org.apache.tools.bzip2.CBZip2OutputStream	\
+	org.apache.tools.bzip2.CRC	\
+	org.apache.tools.bzip2.BZip2Constants
